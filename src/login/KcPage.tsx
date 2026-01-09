5e8bc925175ac6d35f0581f4c3c537e5145c1720
Chung-Hsuan Tsai <paul_tsai@phison.com>
2026-01-09 10:59:51 +0800

    feat: add default pages

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,9 +4,17 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
+
+const Error = lazy(() => import("./pages/Error"));
+const Info = lazy(() => import("./pages/Info"));
+const LoginPageExpired = lazy(() => import("./pages/LoginPageExpired"));
+const LoginUsername = lazy(() => import("./pages/LoginUsername"));
+const LoginPassword = lazy(() => import("./pages/LoginPassword"));
+const Register = lazy(() => import("./pages/Register"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -19,6 +27,62 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "error.ftl":
+                        return (
+                            <Error
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+
+                    case "info.ftl":
+                        return (
+                            <Info
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+
+                    case "login-page-expired.ftl":
+                        return (
+                            <LoginPageExpired
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+
+                    case "login-username.ftl":
+                        return (
+                            <LoginUsername
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+
+                    case "login-password.ftl":
+                        return (
+                            <LoginPassword
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+
+                    case "register.ftl":
+                        return (
+                            <Register
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                                UserProfileFormFields={UserProfileFormFields}
+                                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+                            />
+                        );
+
                     default:
                         return (
                             <DefaultPage
---
+++ src/login/pages/LoginPassword.tsx
@@ -0,0 +1,169 @@
+/**
+ * Password step (login-password.ftl) for flows where username is already captured.
+ * Adds conditional WebAuthn passkey authenticate section when enabled.
+ */
+import type { JSX } from "keycloakify/tools/JSX";
+import { useState } from "react";
+import { kcSanitize } from "keycloakify/lib/kcSanitize";
+import { clsx } from "keycloakify/tools/clsx";
+import { useIsPasswordRevealed } from "keycloakify/tools/useIsPasswordRevealed";
+import { getKcClsx, type KcClsx } from "keycloakify/login/lib/kcClsx";
+import type { PageProps } from "keycloakify/login/pages/PageProps";
+import type { KcContext } from "../KcContext";
+import type { I18n } from "../i18n";
+import { useScript } from "keycloakify/login/pages/LoginPassword.useScript";
+
+export default function LoginPassword(props: PageProps<Extract<KcContext, { pageId: "login-password.ftl" }>, I18n>) {
+    const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
+
+    const { kcClsx } = getKcClsx({
+        doUseDefaultCss,
+        classes
+    });
+
+    const { realm, url, messagesPerField, enableWebAuthnConditionalUI, authenticators } = kcContext;
+
+    const { msg, msgStr } = i18n;
+
+    const [isLoginButtonDisabled, setIsLoginButtonDisabled] = useState(false);
+
+    const webAuthnButtonId = "authenticateWebAuthnButton";
+
+    useScript({
+        webAuthnButtonId,
+        kcContext,
+        i18n
+    });
+
+    return (
+        <Template
+            kcContext={kcContext}
+            i18n={i18n}
+            doUseDefaultCss={doUseDefaultCss}
+            classes={classes}
+            headerNode={msg("doLogIn")}
+            displayMessage={!messagesPerField.existsError("password")}
+        >
+            <div id="kc-form">
+                <div id="kc-form-wrapper">
+                    <form
+                        id="kc-form-login"
+                        onSubmit={() => {
+                            setIsLoginButtonDisabled(true);
+                            return true;
+                        }}
+                        action={url.loginAction}
+                        method="post"
+                    >
+                        <div className={clsx(kcClsx("kcFormGroupClass"), "no-bottom-margin")}>
+                            <hr />
+                            <label htmlFor="password" className={kcClsx("kcLabelClass")}>
+                                {msg("password")}
+                            </label>
+
+                            <PasswordWrapper kcClsx={kcClsx} i18n={i18n} passwordInputId="password">
+                                <input
+                                    tabIndex={2}
+                                    id="password"
+                                    className={kcClsx("kcInputClass")}
+                                    name="password"
+                                    type="password"
+                                    autoFocus
+                                    autoComplete="on"
+                                    aria-invalid={messagesPerField.existsError("username", "password")}
+                                />
+                            </PasswordWrapper>
+
+                            {messagesPerField.existsError("password") && (
+                                <span
+                                    id="input-error-password"
+                                    className={kcClsx("kcInputErrorMessageClass")}
+                                    aria-live="polite"
+                                    dangerouslySetInnerHTML={{
+                                        __html: kcSanitize(messagesPerField.get("password"))
+                                    }}
+                                />
+                            )}
+                        </div>
+                        <div className={kcClsx("kcFormGroupClass", "kcFormSettingClass")}>
+                            <div id="kc-form-options" />
+                            <div className={kcClsx("kcFormOptionsWrapperClass")}>
+                                {realm.resetPasswordAllowed && (
+                                    <span>
+                                        <a tabIndex={5} href={url.loginResetCredentialsUrl}>
+                                            {msg("doForgotPassword")}
+                                        </a>
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        <div id="kc-form-buttons" className={kcClsx("kcFormGroupClass")}>
+                            <input
+                                tabIndex={4}
+                                className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonBlockClass", "kcButtonLargeClass")}
+                                name="login"
+                                id="kc-login"
+                                type="submit"
+                                value={msgStr("doLogIn")}
+                                disabled={isLoginButtonDisabled}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            {enableWebAuthnConditionalUI && (
+                <>
+                    <form id="webauth" action={url.loginAction} method="post">
+                        <input type="hidden" id="clientDataJSON" name="clientDataJSON" />
+                        <input type="hidden" id="authenticatorData" name="authenticatorData" />
+                        <input type="hidden" id="signature" name="signature" />
+                        <input type="hidden" id="credentialId" name="credentialId" />
+                        <input type="hidden" id="userHandle" name="userHandle" />
+                        <input type="hidden" id="error" name="error" />
+                    </form>
+
+                    {authenticators !== undefined && authenticators.authenticators.length !== 0 && (
+                        <>
+                            <form id="authn_select" className={kcClsx("kcFormClass")}>
+                                {authenticators.authenticators.map((authenticator, i) => (
+                                    <input key={i} type="hidden" name="authn_use_chk" readOnly value={authenticator.credentialId} />
+                                ))}
+                            </form>
+                        </>
+                    )}
+                    <br />
+
+                    <input
+                        id={webAuthnButtonId}
+                        type="button"
+                        className={kcClsx("kcButtonClass", "kcButtonDefaultClass", "kcButtonBlockClass", "kcButtonLargeClass")}
+                        value={msgStr("passkey-doAuthenticate")}
+                    />
+                </>
+            )}
+        </Template>
+    );
+}
+
+function PasswordWrapper(props: { kcClsx: KcClsx; i18n: I18n; passwordInputId: string; children: JSX.Element }) {
+    const { kcClsx, i18n, passwordInputId, children } = props;
+
+    const { msgStr } = i18n;
+
+    const { isPasswordRevealed, toggleIsPasswordRevealed } = useIsPasswordRevealed({ passwordInputId });
+
+    return (
+        <div className={kcClsx("kcInputGroup")}>
+            {children}
+            <button
+                type="button"
+                className={kcClsx("kcFormPasswordVisibilityButtonClass")}
+                aria-label={msgStr(isPasswordRevealed ? "hidePassword" : "showPassword")}
+                aria-controls={passwordInputId}
+                onClick={toggleIsPasswordRevealed}
+            >
+                <i className={kcClsx(isPasswordRevealed ? "kcFormPasswordVisibilityIconHide" : "kcFormPasswordVisibilityIconShow")} aria-hidden />
+            </button>
+        </div>
+    );
+}
